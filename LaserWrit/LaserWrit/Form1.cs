b869e00b4b10ce7e0c81e3c91314a4d14a4466df
William Manning <manning.327@osu.edu>
2012-10-14 16:27:02 -0400

    First Draft
    This update has the text box and the create file button along with the
    options list.  Nothing has been implimented.  The button simply creates
    a message box with the text of the text box.

---
+++ LaserWrit/LaserWrit/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaserWrit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            // TODO Create the file based on the input text box.
+            MessageBox.Show(inputTextBox.Text);
+        }
+    }
+}
